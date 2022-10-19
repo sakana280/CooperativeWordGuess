e34f6a0cb32f11c373a480cf3ecd9dd2d9021a81
Mark Foreman <sakana280@gmail.com>
2022-10-19 21:36:51 +1030

    Added word lists in preparation for spell check.

---
+++ Words/Words.cs
@@ -0,0 +1,48 @@
+﻿using System.Reflection;
+
+namespace CooperativeWordGuess.Words
+{
+    public class Words
+    {
+        // Use a HashSet for word list storage because lookup (ie spell checks, frequent!) will be O(1).
+        // Generating a random word (from randmom index 1..max) will be O(N) but less frequent, and N~1000 anyway.
+        private readonly Dictionary<int, HashSet<string>> _words;
+        private readonly Random _random = new();
+
+        public Words()
+        {
+            _words = new(GetWordLists(3, 8));
+        }
+
+        public bool IsKnown(string word)
+        {
+            return _words.TryGetValue(word?.Length ?? 0, out var wordList) && wordList.Contains(word!);
+        }
+
+        public string GetRandom(int length)
+        {
+            var wordList = _words[length];
+            var index = _random.Next(wordList.Count);
+            return wordList.ElementAt(index);
+        }
+
+        private IEnumerable<KeyValuePair<int, HashSet<string>>> GetWordLists(int length1, int length2)
+        {
+            for (var length = length1; length <= length2; length++)
+                yield return new(length, GetWordList(length));
+        }
+
+        private HashSet<string> GetWordList(int length)
+        {
+            var resourceName = $"{typeof(Words).Namespace}.Length{length}.txt";
+            var assembly = Assembly.GetExecutingAssembly();
+            using var stream = assembly.GetManifestResourceStream(resourceName);
+            using var reader = new StreamReader(stream!);
+            var contents = reader.ReadToEnd();
+            var words = contents.Split("\r\n".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+            var badLength = words.FirstOrDefault(w => w.Length != length);
+            if (badLength != null) throw new InvalidDataException($"Word not of length {length}: {badLength}");
+            return new HashSet<string>(words, StringComparer.InvariantCultureIgnoreCase);
+        }
+    }
+}
---
+++ Words/about.md
@@ -0,0 +1,21 @@
+# About these word lists
+These word lists were generated with the [word-extract-tool](word-extract-tool.html)
+using public domain sources, from a time before naughty words were invented:
+ - [Mary Wollstonecraft Shelley's _Frankenstein_](https://www.gutenberg.org/ebooks/42324)
+ - [Jane Austen's _Pride and Prejudice_](https://www.gutenberg.org/ebooks/1342)
+ - [Arthur Conan Doyle's _The Adventures of Sherlock Holmes_](https://www.gutenberg.org/ebooks/1661)
+ - [Louisa May Alcott's _Little Women_](https://www.gutenberg.org/ebooks/37106)
+ - [Bram Stoker's _Dracula_](https://www.gutenberg.org/ebooks/345)
+
+ # License to word lists
+The following lightly curated word lists alone are licensed under 
+[CC0 1.0](https://creativecommons.org/publicdomain/zero/1.0/),
+ie public domain, no copyright.
+- Length3.txt (224 words)
+- Length4.txt (851 words)
+- Length5.txt (1216 words)
+- Length6.txt (1517 words)
+- Length7.txt (1610 words)
+- Length8.txt (1347 words)
+
+All other files in this project are covered by the project license.
